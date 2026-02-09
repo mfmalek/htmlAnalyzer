c20e6fbb09e9439e98fc7314c639468dae875aa5
Malek <marcusfmalek@hotmail.com>
2026-02-09 02:10:38 -0300

    Added Options Menu + HTML Download

--- htmlAnalyzer/src/controller/CtrlProgram.java
+++ htmlAnalyzer/src/controller/CtrlProgram.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Scanner;
 
-import model.htmlAnalyzer;
+import model.HtmlAnalyzer;
 
 public class CtrlProgram {
 	public static void main(String[] args) {
@@ -13,14 +13,54 @@ public class CtrlProgram {
 		System.out.print("Insert a URL: ");
 		String url = input.nextLine();
 		
-		htmlAnalyzer html = new htmlAnalyzer();
+		HtmlAnalyzer html = new HtmlAnalyzer();
 		
-		try {
-			System.out.println(html.dataExtractor(url));
-		} catch(URISyntaxException e) {
-			System.out.println("Invalid URL (" + e.getReason() + ").");
-		} catch (IOException e) {
-			System.out.println("Failed to connect to the URL host (" + html.getHost() + ").");
+		System.out.println("===== SELECT WHAT YOU WANT TO DO =====\n");
+		System.out.println("==> Data Extraction");
+		System.out.println("\t1. HTML Data Extractor");
+		System.out.println("\t2. HTML Head Data Extractor");
+		System.out.println("\t3. HTML Body Data Extractor");
+		System.out.println("==> Data Download");
+		System.out.println("\t4. HTML Data Download");
+		System.out.println("\t5. HTML Head Data Download");
+		System.out.println("\t6. HTML Body Data Download");
+		System.out.println("==> Miscellaneous");
+		System.out.println("\t7. HTML Tag Counter");
+		System.out.print("\nYour choice: ");
+		int option = input.nextInt();
+		
+		switch(option) {
+			case 1:
+				try {
+					System.out.println(html.htmlDataExtractor(url));
+				} catch(URISyntaxException e) {
+					System.out.println("Invalid URL (" + e.getReason() + ").");
+				} catch (IOException e) {
+					System.out.println("Failed to connect to the URL host (" + html.getHost() + ").");
+				}
+				break;
+			case 2:
+				// CODE
+			case 3:
+				// CODE
+			case 4:
+				try {
+					html.htmlDataDownload(html.htmlDataExtractor(url));
+					System.out.println("File Downloaded");
+				} catch(URISyntaxException e) {
+					e.printStackTrace();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+				break;
+			case 5:
+				// CODE
+			case 6:
+				// CODE
+			case 7:
+				// CODE
+			default:
+				System.out.println("Please type a number between 1 - 9.");
 		}
 	}
 }
---
+++ htmlAnalyzer/src/model/HtmlAnalyzer.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class HtmlAnalyzer {
+	private String host;
+	
+	public String htmlDataExtractor(String hyperlink) throws URISyntaxException, IOException {
+		if(!hyperlink.startsWith("http://") && !hyperlink.startsWith("https://")) {
+			hyperlink = "https://" + hyperlink;
+		}
+		
+		URI uri = new URI(hyperlink);
+		if(!uri.isAbsolute()) {
+			throw new URISyntaxException(hyperlink, "URI is not absolute.");
+		}
+		
+		host = uri.getHost();
+		
+		URL url = uri.toURL();
+		StringBuilder htmlBuilder = new StringBuilder();
+
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+		    String line;
+		    
+		    while ((line = reader.readLine()) != null) {
+		        htmlBuilder.append(line).append("\n");
+		    }
+		}
+		return htmlBuilder.toString();
+	}
+	
+	public void htmlDataDownload(String htmlData) throws IOException {
+		String userHomePath = System.getProperty("user.home");
+		Path htmlDataFilePath = Paths.get(userHomePath).resolve("Downloads").resolve("HTML Data.txt");
+		
+	    try(BufferedWriter writer = new BufferedWriter(new FileWriter(htmlDataFilePath.toString()))) {
+	    	writer.write(htmlData);
+	    }
+	}
+
+	
+	public String getHost() {
+		return host;
+	}
+}
